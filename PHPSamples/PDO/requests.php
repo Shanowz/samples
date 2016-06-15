a2edc3689e82927db273f6cd1a726ce1932c934d
webco12 <webco12@PRIM2010-03.fapse.priv>
2016-06-15 10:50:20 +0200

    adding iterate on request.php / SimpleRequest() adding new function: ClientRequest

--- PHPSamples/PDO/requests.php
+++ PHPSamples/PDO/requests.php
@@ -7,10 +7,23 @@ function SimpleRequest(){
         $connexion = $pdo->get_connection();
         $requ = "SELECT prenom, nom FROM employes";
         $pdo_statement = $connexion->query($requ);
-        $cal =  $pdo_statement->fetch(PDO::FETCH_OBJ);
-        print_r($cal);
+        $cal =  $pdo_statement->fetchAll(PDO::FETCH_OBJ);
 
+        //itarate over the array $cal
+        foreach($cal as $item){
+            $line = (array)$item;
+            foreach($line as $col){
+                echo "  ".$col;
+            }
+            echo "<br>";
+        }
     }catch(PDOException $e){
         echo "requête impossile : ".$e;
     }
+}
+
+function ClientRequest(){
+    $pdo = DBConnect::get_instance('connection.ini');
+    $connexion = $pdo->get_connection();
+
 }
